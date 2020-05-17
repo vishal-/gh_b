a5c8e2e03b7a40b2231ea3f683ee3624c53bd063
Vishal Gupta <ims.vishal@gmail.com>
2020-05-17 19:41:00 +0530

    DB configuration updated

--- config/app.config.js
+++ config/app.config.js
@@ -4,13 +4,23 @@ export const SERVER = {
   PORT: 4040
 };
 
-export const DB = {
-  host: "192.168.1.48",
-  port: "3306",
-  username: "dev",
-  password: "dev1234",
-  database: "gh_b"
-};
+export const DB = (() => {
+  if (process.env.NODE_ENV && process.env.NODE_ENV !== "development") {
+    const dbconfig = process.env.GH_DB_CONFIG;
+    if (dbconfig) {
+      return JSON.parse(dbconfig);
+    } else {
+      console.error(`DB configuration string not found in environment`);
+    }
+  } else {
+    return {
+      host: "192.168.1.48",
+      schema: "gh_b",
+      username: "dev",
+      password: "dev1234"
+    };
+  }
+})();
 
 export const LOG = {
   PATH: `${HOME_DIR}/logs/gh`,
--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -2,10 +2,10 @@ import Sequelize from "sequelize";
 import { DB } from "./app.config";
 import logger from "./logger.config";
 
-export const sequelize = new Sequelize(DB.database, DB.username, DB.password, {
+export const sequelize = new Sequelize(DB.schema, DB.username, DB.password, {
   host: DB.host,
   dialect: "mysql",
-  logging: (message) => {
+  logging: message => {
     logger.debug(message);
   }
 });
