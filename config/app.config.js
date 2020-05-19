d3b6101067d3f7793de4e4296269700a9266b24a
Vishal Gupta <ims.vishal@gmail.com>
2020-05-19 18:01:00 +0530

    Change in db configuration method

--- config/app.config.js
+++ config/app.config.js
@@ -4,24 +4,6 @@ export const SERVER = {
   PORT: 4040
 };
 
-export const DB = (() => {
-  if (process.env.NODE_ENV && process.env.NODE_ENV !== "development") {
-    const dbconfig = process.env.GH_DB_CONFIG;
-    if (dbconfig) {
-      return JSON.parse(dbconfig);
-    } else {
-      console.error(`DB configuration string not found in environment`);
-    }
-  } else {
-    return {
-      host: "192.168.1.48",
-      schema: "gh_b",
-      username: "dev",
-      password: "dev1234"
-    };
-  }
-})();
-
 export const LOG = {
   PATH: `${HOME_DIR}/logs/gh`,
   FILENAME: "gh_debug.log",
--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -1,8 +1,29 @@
 import Sequelize from "sequelize";
-import { DB } from "./app.config";
 import logger from "./logger.config";
 
-logger.info(`DB details found -- Schema: ${DB.schema} & Host: ${DB.host}`);
+const getDBConfiguration = () => {
+  if (process.env.NODE_ENV && process.env.NODE_ENV !== "development") {
+    const dbconfig = process.env.GH_DB_CONFIG;
+
+    if (dbconfig) {
+      const db_config = JSON.parse(dbconfig);
+      logger.info(
+        `DB configuraiton: HOST=${db_config.host} & SCHEMA=${db_config.schema}`
+      );
+      return db_config;
+    }
+
+    logger.error(`DB configuration string not found in the environment`);
+  }
+  return {
+    host: "192.168.1.48",
+    schema: "gh_b",
+    username: "dev",
+    password: "dev1234"
+  };
+};
+
+const DB = getDBConfiguration();
 
 export const sequelize = new Sequelize(DB.schema, DB.username, DB.password, {
   host: DB.host,
